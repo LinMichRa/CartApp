7de23956b3fca3a90783f2e94e7939fec31321a3
LinMichRa <lramirezd2@ucentral.edu.co>
2024-08-15 07:32:19 -0500

    Adding products functionality working

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../models/cartItem';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
+import { CartComponent } from './cart/cart.component';
 import { CatalogComponent } from './catalog/catalog.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent,CartComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit{
   products: Product[] = [];
+
+  items: CartItem[] = [];
+
   constructor(private service: ProductService){
 
   }
   ngOnInit(): void {
     this.products = this.service.findAll();
   }
+
+  onAddCart(product:Product){
+    this.items = [...this.items,{product: {...product},quantity:1}];
+  }
 }
+
