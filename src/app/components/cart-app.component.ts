98046a1a7896bbf643d1163db69a92a41956e197
LinMichRa <lramirezd2@ucentral.edu.co>
2024-08-15 12:36:16 -0500

    Some models added

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CartItem } from '../models/cartItem';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
-import { CartComponent } from './cart/cart.component';
+import { CartModalComponent } from './cart-modal/cart-modal.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'cart-app',
   standalone: true,
-  imports: [CatalogComponent,CartComponent,NavbarComponent],
+  imports: [CatalogComponent,CartModalComponent,NavbarComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit{
@@ -62,7 +62,7 @@ export class CartAppComponent implements OnInit{
     sessionStorage.setItem('cart', JSON.stringify(this.items));
   }
 
-  openCart():void{
+  openCloseCart():void{
     this.showCart = !this.showCart;
   }
 }
